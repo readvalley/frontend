57de2b93ffe19c6e941268a8cffaef4b23eee4eb
Junho Yeo <hanaro0704@gmail.com>
2020-09-20 16:01:48 +0900

    feat: api & mockup for Series

--- src/pages/Series.tsx
+++ src/pages/Series.tsx
@@ -1,5 +1,6 @@
 import { css } from "@emotion/core";
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 import {
   ContentTitle,
@@ -18,18 +19,21 @@ import WritingListItem from "../components/WritingListItem";
 import Comment from "../components/Comment";
 import { Link } from "react-router-dom";
 
-const getSeriesData = (id: string): IDisplaySeries => {
+const getSeriesData = async (id: string) => {
+  const { data: { book } } = await axios.get('http://localhost:5000/book/5f66ebb327e6032ba0d83557');
+  console.log(book)
+
   return {
-    title: "전생엔 흔한 고교생이였던 내가 이번생에도 고교생",
-    image: "https://cdn.clien.net/web/api/file/F01/10589109/55661ee1189447.jpg",
-    hearts: 8282,
+    title: book.title,
+    image: book.thumbnailURL,
+    hearts: 2,
     hearted: false,
     creator: {
-      name: "유은혜",
+      name: '여준호',
       _id: "2097",
     },
     category: "경제 · 자기계발",
-    intro: `이에 따라 창작자의 힘이 상당히 위축되어있습니다. 책의 단가를 정하는 과정에서 출판사와 서점이 갑의 위치에 있기에, 창작물에 정당한 단가를 책정하지 못합니다.`,
+    intro: book.description,
     _id: "28237",
     writings: [
       {
@@ -54,19 +58,19 @@ const getSeriesData = (id: string): IDisplaySeries => {
     comments: [
       {
         date: new Date(),
-        content: "혁명이다",
+        content: "감동적이군요..",
         author: {
           name: "revolutionary",
-          image: "http://image.yes24.com/goods/91869001/",
+          image: "https://avatars1.githubusercontent.com/u/35295182?v=4",
         },
         _id: "D2D2D2",
       },
       {
         date: new Date(),
-        content: "혁명이다",
+        content: "대단한 사람이네요 ㅎㅎ",
         author: {
-          name: "revolutionary",
-          image: "http://image.yes24.com/goods/91869001/",
+          name: "cokia",
+          image: "https://avatars3.githubusercontent.com/u/24792377?v=4",
         },
         _id: "D2D2D3",
       },
@@ -78,7 +82,13 @@ const getSeriesData = (id: string): IDisplaySeries => {
 const Series: React.FC<{ id: string }> = ({ id }) => {
   const [seriesData, setSeriesData] = useState<IDisplaySeries>();
   useEffect(() => {
-    setSeriesData(getSeriesData(id));
+    const getData = async () => {
+      const data = await getSeriesData(id);
+      console.log(data)
+      setSeriesData(data as any);
+    };
+
+    getData();
   }, [id]);
   if (!seriesData) return <></>;
   return (
@@ -149,10 +159,8 @@ const Series: React.FC<{ id: string }> = ({ id }) => {
           </Detail>
         </>
       )}
-      <SubgroupTitle>회차</SubgroupTitle>
-      {seriesData.writings?.map((d) => (
-        <WritingListItem {...d} key={d._id} />
-      ))}
+      <SubgroupTitle>내용</SubgroupTitle>
+      <div></div>
       <SubgroupTitle
         css={css`
           margin-bottom: 10px;
